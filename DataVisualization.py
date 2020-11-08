c67be192b1b56ccf61634acf5fd159b8ce5ef381
amyscich <akmyscich@hotmail.com>
2020-11-08 15:34:46 -0600

    Added data source, functions.py to ImageRaster.py, and added DataVisualization.py

---
+++ DataVisualization.py
@@ -0,0 +1,3 @@
+# (b.ii) (2 points) Feature exploration. Provide visualizations of the features with respect
+# to the outcome (e.g., overlaying histograms, scatter plots), and quantify associations between
+# the features and the outcome.
---
+++ main.py
@@ -0,0 +1,7 @@
+from COVID19Classifier.ImageRaster import ImageRaster
+
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
